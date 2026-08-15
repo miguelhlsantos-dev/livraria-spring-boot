4629b6065c8d5bd010aeaeb2895799353f8654cb
Miguel Santos <miguelluzhlds@gmail.com>
2026-08-15 18:33:07 -0300

    feat: implements ErrorResponseDTO

--- src/main/java/com/exemplo/livraria/exception/GlobalExceptionHandler.java
+++ src/main/java/com/exemplo/livraria/exception/GlobalExceptionHandler.java
@@ -1,26 +1,43 @@
 package com.exemplo.livraria.exception;
 
+import com.exemplo.livraria.dto.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(LivroNaoEncontradoException.class)
-    public ResponseEntity<String> tratarLivroNaoEncontrado(LivroNaoEncontradoException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Livro não encontrado");
+    public ResponseEntity<ErrorResponseDTO> tratarLivroNaoEncontrado(LivroNaoEncontradoException exception){
+        ErrorResponseDTO erro = new ErrorResponseDTO();
+        erro.setTimestamp(LocalDateTime.now());
+        erro.setMensagem(exception.getMessage());
+        erro.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
     @ExceptionHandler(AutorNaoEncontradoException.class)
-    public ResponseEntity<String> tratarAutorNaoEncontrado(AutorNaoEncontradoException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Autor não encontrado");
+    public ResponseEntity<ErrorResponseDTO> tratarAutorNaoEncontrado(AutorNaoEncontradoException exception){
+       ErrorResponseDTO erro = new ErrorResponseDTO();
+       erro.setMensagem(exception.getMessage());
+       erro.setStatus(HttpStatus.NOT_FOUND.value());
+       erro.setTimestamp(LocalDateTime.now());
+       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
+
     @ExceptionHandler(ClienteNaoEncontradoException.class)
-    public ResponseEntity<String> tratarClienteNaoEncontrado (ClienteNaoEncontradoException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+    public ResponseEntity<ErrorResponseDTO> tratarClienteNaoEncontrado (ClienteNaoEncontradoException exception){
+        ErrorResponseDTO erro = new ErrorResponseDTO();
+        erro.setMensagem(exception.getMessage());
+        erro.setTimestamp(LocalDateTime.now());
+        erro.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
 }
